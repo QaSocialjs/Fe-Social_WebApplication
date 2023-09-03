87a21d9da14a106dcbfb57b5e7d35b2812fe65fd
NNQA <anhla12h@gmail.com>
2023-09-03 12:16:29 +0700

    feat: finished fe post

--- fe/fe-fb/src/routes/UI/Home/post/Post.tsx
+++ fe/fe-fb/src/routes/UI/Home/post/Post.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import classes from "./Post.module.css";
 import avt from "../../../../assets/avatar.jpg";
 import { IconTweetData } from "../../../../lib/Data/Tweet";
@@ -6,6 +6,19 @@ import { HeroIcon } from "../../../../components/Icon/hero-icon";
 import Button from "../../../../components/Button";
 
 function Post() {
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  const handleInputChange = (event: any) => {
+    const selectedFile = event.target.files[0];
+    console.log("Selected file:", selectedFile);
+  };
+
+  const handleClick = () => {
+    if (fileInputRef.current) {
+      fileInputRef.current.click();
+    }
+  };
+
   return (
     <div className={classes.Post}>
       <div>
@@ -17,9 +30,25 @@ function Post() {
         </div>
         <div className={classes.grActionTweet}>
           <div className={classes.grICon}>
-            {IconTweetData.map(({ ...icondata }, idx) => (
-              <HeroIcon iconName={icondata.iconName} key={idx}></HeroIcon>
-            ))}
+            {IconTweetData.map(({ ...icondata }, idx) => {
+              if (icondata.iconName === "PhotoIcon") {
+                return (
+                  <div className={classes.openFolder} onClick={handleClick}>
+                    <HeroIcon iconName={icondata.iconName} key={idx}></HeroIcon>
+                    <input
+                      type="file"
+                      style={{ display: "none" }}
+                      ref={fileInputRef}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                );
+              } else {
+                return (
+                  <HeroIcon iconName={icondata.iconName} key={idx}></HeroIcon>
+                );
+              }
+            })}
           </div>
           <div className={classes.grbutton}>
             <Button name="Tweet"></Button>
