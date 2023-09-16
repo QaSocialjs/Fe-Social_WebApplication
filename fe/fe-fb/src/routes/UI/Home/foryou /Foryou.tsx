cc7cf7ad31d47979bf2fd59784f70a99e900d9e8
NNQA <anhla12h@gmail.com>
2023-09-16 14:48:38 +0700

    feat(foryou): add file css, and add feature for foryou page

--- fe/fe-fb/src/routes/UI/Home/foryou /Foryou.tsx
+++ fe/fe-fb/src/routes/UI/Home/foryou /Foryou.tsx
@@ -1,7 +1,31 @@
-import React from "react";
-
-function Foryou() {
-  return <div>Foryou</div>;
-}
+import { useState } from "react";
+import Button from "../../../../components/Button";
+import MainContainer from "../../../../components/Home/main-container";
+import Card from "../../../../components/UI/Card";
+import Spinner from "../../../../components/UI/spinner";
+import { datahome } from "../../../../lib/Data/Home";
+import classes from "./Foryou.module.css";
+const Foryou = (): JSX.Element => {
+  const [load, setLoad] = useState<Boolean>(false);
+  const handleLoad = () => {
+    setLoad((pre) => !pre);
+  };
+  return (
+    <MainContainer>
+      {datahome.map(({ ...data }, idx) => (
+        <div key={idx}>
+          <Card {...data}></Card>
+        </div>
+      ))}
+      <div className={classes.loader}>
+        {load ? (
+          <Spinner setLoad={setLoad}></Spinner>
+        ) : (
+          <Button name="Load more" onClick={handleLoad}></Button>
+        )}
+      </div>
+    </MainContainer>
+  );
+};
 
 export default Foryou;
