260da8562cb40378f2634115c111adfa16785985
NNQA <anhla12h@gmail.com>
2023-12-15 15:20:23 +0700

    change be

--- be/controllers/usersController.js
+++
@@ -1,4 +0,0 @@
-export const updateUsers = (req, res) => {
-  console.log("req", req);
-  console.log(req.params.id);
-};
--- be/routes/home.js
+++
@@ -1,9 +0,0 @@
-import Express from "express";
-import { verifyToken } from "../middlewares/auth.js";
-const router = Express.Router();
-
-const a = () => {
-  console.log("adsa");
-};
-router.get("/Home", verifyToken, a);
-export default router;
