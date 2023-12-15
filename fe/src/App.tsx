10552ebf83ba16301f34fde7955c46e80b6685d1
NNQA <anhla12h@gmail.com>
2023-12-15 15:34:52 +0700

    update: update app.tsx

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "./routes/home";
-import Subject from "./routes/subject";
 import Login from "./routes/login";
 import LangProvider from "./lib/context/languagecontex";
 import { ThemeProvider } from "./lib/context/theme.context";
+import { ApiClient } from "@lib/services/ApiClient";
+import configOptions from "@lib/services/configOption";
+import SignUp from "./routes/signup";
 
 function App() {
+  ApiClient.use(configOptions);
   return (
     <LangProvider>
       <ThemeProvider>
@@ -15,6 +17,7 @@ function App() {
           <Route path="subject" element={<Subject />} />
         </Route> */}
           <Route path={`/login`} element={<Login></Login>}></Route>
+          <Route path={`/signup`} element={<SignUp></SignUp>}></Route>
         </Routes>
       </ThemeProvider>
     </LangProvider>
