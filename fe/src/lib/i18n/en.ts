3e9f8ebff7d055d323c13f24346147c1a125656b
NNQA <anhla12h@gmail.com>
2023-12-15 15:25:44 +0700

    feat(i18n): add translation for signup page

--- fe/src/lib/i18n/en.ts
+++ fe/src/lib/i18n/en.ts
@@ -13,6 +13,31 @@ export const en = {
           invalid: "Password must be at least 8 characters",
         },
       },
+      signup: {
+        header: "Signup",
+        email: {
+          label: "Email address",
+          required: "Email is a required field",
+          invalid: "Invalid email address",
+        },
+        password: {
+          label: "Password",
+          required: "Password is a required field",
+          invalid: "Password must be at least 8 characters",
+        },
+        confirmpassword: {
+          label: "Confirm password",
+          required: "ConfirmPassword is a required field",
+          invalid: "Password not match",
+        },
+        success: "Signup successfully",
+      },
+      footer: {
+        signup: {
+          title: "Already have an account?",
+          link: "Login now",
+        },
+      },
     },
     footer: {
       text: "All rights reserved.",
--- fe/src/lib/i18n/vi.ts
+++ fe/src/lib/i18n/vi.ts
@@ -13,6 +13,31 @@ export const vi = {
           invalid: "Mật khẩu cần có 8 ký tự",
         },
       },
+      signup: {
+        header: "Đăng ký",
+        email: {
+          label: "Email address",
+          required: "Email được yêu cầu",
+          invalid: "Địa chỉ email không hợp lệ",
+        },
+        password: {
+          label: "Password",
+          required: "Password được yêu cầu",
+          invalid: "Độ dài mật khẩu cần ít nhất 8 ký tự",
+        },
+        confirmpassword: {
+          label: "Confirm Password",
+          required: "ConfirmPassword được yêu cầu",
+          invalid: "Mật khẩu không đúng",
+        },
+        success: "Đăng ký thành công",
+      },
+      footer: {
+        signup: {
+          title: "Đã có tài khoản?",
+          link: "Đăng nhập ngay",
+        },
+      },
     },
     footer: {
       text: "Đã đăng ký bản quyền.",
