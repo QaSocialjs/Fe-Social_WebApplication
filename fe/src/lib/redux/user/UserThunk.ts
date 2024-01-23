7d4e50c30797e57f369138d033ada0de02bca759
NNQA <anhla12h@gmail.com>
2024-01-23 13:16:09 +0700

    update: update code of userThunk

--- fe/src/lib/redux/user/UserThunk.ts
+++ fe/src/lib/redux/user/UserThunk.ts
@@ -1,10 +1,22 @@
 import { User } from "@lib/models/User";
 import { ApiClient } from "@lib/services/ApiClient";
-import { ApiError } from "@lib/services/ErrorApi";
+import { JsonPatch } from "@lib/utils/service.assetInfo";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 
 interface values extends User {
   confirmpassword: string;
+}
+interface updateValues
+  extends Omit<
+    User,
+    "firstName" | "lastName" | "age" | "id" | "email" | "password"
+  > {
+  id?: string;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  password?: string;
+  age?: string;
 }
 export const Login = createAsyncThunk(
   "user/login",
@@ -67,12 +79,23 @@ export const verifyAuthentication = createAsyncThunk(
 export const getUser = createAsyncThunk(
   "user/getUser",
   async (values: "", thunkAPI) => {
-    const response = await ApiClient.instance.get("/getUser", {
+    const response: any = await ApiClient.instance.get("/getuser", {
       credentials: "include",
     });
     if (response.isErr()) {
-      const apiError = response.error as ApiError;
-      return thunkAPI.rejectWithValue(apiError);
+      return thunkAPI.rejectWithValue(response);
+    }
+    return thunkAPI.fulfillWithValue(response);
+  }
+);
+export const getUserId = createAsyncThunk(
+  "user/getUserId",
+  async (values: { id: string }, thunkAPI) => {
+    const response = await ApiClient.instance.get(`/getuser/${values.id}`, {
+      credentials: "include",
+    });
+    if (response.isErr()) {
+      return thunkAPI.rejectWithValue(response);
     }
     return thunkAPI.fulfillWithValue(response);
   }
@@ -100,6 +123,88 @@ export const SendMail = createAsyncThunk(
         email: values.email,
       },
     });
+    if (response.isErr()) {
+      return thunkAPI.rejectWithValue(response);
+    }
+    return thunkAPI.fulfillWithValue(response);
+  }
+);
+export const RequestImg = createAsyncThunk(
+  "user/requestimg",
+  async (values: { id: string }, thunkAPI) => {
+    const response = await ApiClient.instance.post(
+      `/upload/${values.id}/avatar`,
+      {
+        credentials: "include",
+      }
+    );
+    if (response.isErr()) {
+      return thunkAPI.rejectWithValue(response);
+    }
+    return thunkAPI.fulfillWithValue(response);
+  }
+);
+
+export const PatchAvatarUser = createAsyncThunk(
+  "user/patchAvatarUser",
+  async (values: { patch: JsonPatch; id: string }, thunkAPI) => {
+    const response = await ApiClient.instance.patch("/patchAvatarUser", {
+      body: {
+        id: values.id,
+        patch: values.patch.jsonpatch,
+      },
+      credentials: "include",
+    });
+    if (response.isErr()) {
+      return thunkAPI.rejectWithValue(response);
+    }
+    return thunkAPI.fulfillWithValue(response);
+  }
+);
+
+export const UpdateUser = createAsyncThunk(
+  "user/updateUser",
+  async (values: updateValues, thunkAPI) => {
+    const response = await ApiClient.instance.put(`/updateUser/${values.id}`, {
+      body: {
+        ...values,
+      },
+      credentials: "include",
+    });
+    if (response.isErr()) {
+      return thunkAPI.rejectWithValue(response);
+    }
+    return thunkAPI.fulfillWithValue(response);
+  }
+);
+
+export const DeleteCityUser = createAsyncThunk(
+  "user/deleteCityUser",
+  async (values: updateValues, thunkAPI) => {
+    const response = await ApiClient.instance.delete(
+      `/deleteCity/${values.id}`,
+      {
+        credentials: "include",
+      }
+    );
+    if (response.isErr()) {
+      return thunkAPI.rejectWithValue(response);
+    }
+    return thunkAPI.fulfillWithValue(response);
+  }
+);
+
+export const CreatWork = createAsyncThunk(
+  "user/createWork",
+  async (values: updateValues, thunkAPI) => {
+    const response = await ApiClient.instance.post(
+      `/createUserWork/${values.id}`,
+      {
+        body: {
+          ...values,
+        },
+      }
+    );
     if (response.isErr()) {
       return thunkAPI.rejectWithValue(response);
     }
