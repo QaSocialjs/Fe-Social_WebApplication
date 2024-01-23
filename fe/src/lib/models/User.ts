5a7630756b892846397ae88faf9f1930d91b935c
NNQA <anhla12h@gmail.com>
2024-01-23 13:06:46 +0700

    add: add models

--- fe/src/lib/models/User.ts
+++ fe/src/lib/models/User.ts
@@ -1,4 +1,8 @@
+import { AssetInfo } from "./AssetInfo";
+import { Work } from "./Work";
+
 export interface User {
+  id: string;
   firstName: string;
   lastName: string;
   email: string;
@@ -7,4 +11,27 @@ export interface User {
   refreshtoken?: string;
   gender?: number;
   age: string;
+  avatar?: AssetInfo;
+  nameCity?: string;
+  city?: City;
+  work?: Work;
+  startWork?: Date;
+  endWork?: Date;
 }
+export interface City {
+  nameCity: string;
+  time: Date;
+}
+export const enum Gender {
+  Male = 1,
+  Female,
+}
+export const genderNames = {
+  [Gender.Male]: "Male",
+  [Gender.Female]: "Female",
+};
+
+export const genders = [
+  { key: 1, value: Gender.Male },
+  { key: 2, value: Gender.Female },
+];
