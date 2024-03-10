a6b20ee7e267d3e5e50a077b767ab649aa02f855
NNQA <anhla12h@gmail.com>
2024-03-10 15:17:54 +0700

    style: style ui main layout

--- fe/src/lib/layouts/layout-main-screen.tsx
+++ fe/src/lib/layouts/layout-main-screen.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import NavigationBar from "@components/Navigation/NavigationBar";
 import { Outlet } from "react-router";
+import BoxAlertProvider from "@lib/context/BoxAlertcontext";
 
 const Mainlayout = (): React.ReactNode => {
   return (
-    <div className="w-full h-full flex flex-col">
-      <NavigationBar />
-      <div className="bg-primary-50">
-        <Outlet></Outlet>
-      </div>
+    <div className="w-screen h-screen">
+      <BoxAlertProvider>
+        <NavigationBar />
+        <div className="bg-primary-50">
+          <Outlet></Outlet>
+        </div>
+      </BoxAlertProvider>
     </div>
   );
 };
