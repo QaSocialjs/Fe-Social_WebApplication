a6e817bc1786c911a46340ee909e4a35377f02ad
NNQA <anhla12h@gmail.com>
2024-03-10 15:20:28 +0700

    change: change app.tsx

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -13,7 +13,18 @@ import { RouterProvider } from "react-aria-components";
 import UserContextProvider from "@lib/context/usercontext";
 import Profile from "./routes/profile/profile";
 import ConfirmAccount from "./routes/ConfirmAccount/ConfirmAccount";
-
+import Post from "./routes/profile/tabfeature/Post";
+import About from "./routes/profile/tabfeature/About/About";
+import OverView from "./routes/profile/tabfeature/About/OverView";
+import Mainlayout from "@lib/layouts/layout-main-screen";
+import PlaceLive from "./routes/profile/tabfeature/About/PlaceLive";
+import ContactAndInfo from "./routes/profile/tabfeature/About/ContactInfo";
+import WorkPlace from "./routes/profile/tabfeature/About/WorkPlace";
+import Friends from "./routes/friends";
+import SocketProvider from "@lib/context/SocketIocontext";
+import LayoutFriend from "@lib/layouts/layout-friend";
+import FriendRequestion from "./routes/friends/FriendRequestion";
+import FriendSuggestion from "./routes/friends/FriendSuggestion";
 function App() {
   ApiClient.use(configOptions);
   let navigate = useNavigate();
@@ -27,20 +38,49 @@ function App() {
               path="/"
               element={
                 <UserContextProvider>
-                  <Home />
+                  <SocketProvider>
+                    <Mainlayout></Mainlayout>
+                  </SocketProvider>
                 </UserContextProvider>
               }
             >
-              <Route path="/videos" element={<SignUp></SignUp>}></Route>
+              <Route index element={<Home />}></Route>
+              <Route path="friends" element={<LayoutFriend />}>
+                <Route index element={<Friends />}></Route>
+                <Route
+                  path="/friends/requests"
+                  element={
+                    <div className="overflow-y-auto">
+                      <FriendRequestion />
+                    </div>
+                  }
+                ></Route>
+                <Route
+                  path="/friends/suggestions"
+                  element={
+                    <div className="overflow-y-auto">
+                      <FriendSuggestion />
+                    </div>
+                  }
+                ></Route>
+              </Route>
+              <Route path=":id/*" element={<Profile />}>
+                <Route index element={<Post />}></Route>
+                <Route path="about" element={<About />}>
+                  <Route index element={<OverView />}></Route>
+                  <Route path="places_lived" element={<PlaceLive />}></Route>
+                  <Route
+                    path="contact_and_basic_info"
+                    element={<ContactAndInfo />}
+                  ></Route>
+                  <Route
+                    path="work_and_education"
+                    element={<WorkPlace />}
+                  ></Route>
+                </Route>
+              </Route>
             </Route>
-            <Route
-              path="/:id"
-              element={
-                <UserContextProvider>
-                  <Profile />
-                </UserContextProvider>
-              }
-            ></Route>
+
             <Route
               path={`/login`}
               element={
